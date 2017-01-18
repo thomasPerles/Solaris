cb852b5b173633d7bf5a6983852c32795422d882
tomtom06 <thomas.perles@gmail.com>
2017-01-18 19:08:33 +0100

    Premier commit .jar .so .txt .jnilib .dylib .dll .java

---
+++ SOLARIS/src/Solaris/Launcher.java
@@ -0,0 +1,14 @@
+package Solaris;
+
+public class Launcher {
+
+	/**
+	 * @author weber
+	 * @param args Parameters of the launcher
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
